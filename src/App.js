f059cd16c6b56e8c8aab74f9268a22baa3957be3
Lukasz <lukasz.maniak22@gmail.com>
2023-09-16 22:07:21 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const images = require.context("./images", true);
-const imageList = images.keys().map((image) => images(image));
-console.log(imageList[2]);
-
-const workersList = {
-  1: {
+const workersList = [
+  {
     name: "Tanya Sinclair",
     role: "UX Engineer",
     quote: `“ I’ve been interested in coding for a while but never taken the jump,
     until now. I couldn’t recommend this course enough. I’m now in the job
     of my dreams and so excited about the future. ”`,
+    imgSrc: require("./images/image-tanya.jpg"),
   },
-  2: {
+  {
     name: "John Tarkpor",
     role: "Junior Front-end",
     quote: `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`,
+    imgSrc: require("./images/image-john.jpg"),
   },
-};
+];
 
 function App() {
   return (
@@ -28,20 +26,32 @@ function App() {
 }
 
 function Worker() {
+  const [slide, setSlide] = useState(0);
+  console.log(slide, workersList.length);
+  function handlePrev() {
+    if (slide > 0) setSlide((s) => s - 1);
+  }
+  function handleNext() {
+    slide < workersList.length - 1
+      ? setSlide((s) => s + 1)
+      : setSlide(workersList.length - 1);
+  }
+
   return (
-    <div>
+    <div className="worker-container">
       <div className="quote-text-container">
-        <p>{workersList[1].quote}</p>
+        <p>{workersList[+slide].quote}</p>
         <p>
-          <b>{workersList[1].name}</b> <span>{workersList[1].role}</span>
+          <b>{workersList[+slide].name}</b>{" "}
+          <span>{workersList[+slide].role}</span>
         </p>
       </div>
       <div className="image-container">
-        <img src={imageList[4]} alt="" />
-        <button>
+        <img className="worker-image" src={workersList[+slide].imgSrc} alt="" />
+        <button onClick={() => handlePrev()} className="prev-btn">
           <img src="" alt="" />
         </button>
-        <button>
+        <button onClick={() => handleNext()} className="next-btn">
           <img src="" alt="" />
         </button>
       </div>
