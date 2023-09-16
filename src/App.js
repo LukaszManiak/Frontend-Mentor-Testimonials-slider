66f4e39319197d9210ac23bbf95ecd252ed9a779
Lukasz <lukasz.maniak22@gmail.com>
2023-09-16 21:12:38 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const images = require.context("./images", true);
+const imageList = images.keys().map((image) => images(image));
+console.log(imageList[2]);
+
+const workersList = {
+  1: {
+    name: "Tanya Sinclair",
+    role: "UX Engineer",
+    quote: `“ I’ve been interested in coding for a while but never taken the jump,
+    until now. I couldn’t recommend this course enough. I’m now in the job
+    of my dreams and so excited about the future. ”`,
+  },
+  2: {
+    name: "John Tarkpor",
+    role: "Junior Front-end",
+    quote: `“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”`,
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="main-container">
+      <Worker />
+    </div>
+  );
+}
+
+function Worker() {
+  return (
+    <div>
+      <div className="quote-text-container">
+        <p>{workersList[1].quote}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>{workersList[1].name}</b> <span>{workersList[1].role}</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="image-container">
+        <img src={imageList[4]} alt="" />
+        <button>
+          <img src="" alt="" />
+        </button>
+        <button>
+          <img src="" alt="" />
+        </button>
+      </div>
     </div>
   );
 }
